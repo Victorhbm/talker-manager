0fa63dd61d4db6ffa4e6c49826abbcfea88f4458
victorhbm <victor.hugofla@hotmail.com>
2022-03-18 18:25:11 -0300

    remove trybe files

--- index.js
+++ index.js
@@ -22,13 +22,7 @@ const {
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK_STATUS = 200;
 const PORT = '3000';
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
-});
 
 app.get('/talker', getTalkers);
 
