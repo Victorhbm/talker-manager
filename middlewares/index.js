6a7fbd41add4244948a8fd63377fe6c153c13744
victorhbm <victor.hugofla@hotmail.com>
2022-03-18 11:41:16 -0300

    feat(middlewares): add middleware to
    - Authenticator Token
    - Validate Name
    - Validate Age
    - Validate Talk WatchedAt
    - Validate Talk Rate

--- middlewares/index.js
+++ middlewares/index.js
@@ -31,4 +31,96 @@ function validatePassword(req, res, next) {
   next();
 }
 
-module.exports = { validateEmail, validatePassword };
+function authenticationToken(req, res, next) {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+
+  if (authorization.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+
+  next();
+}
+
+function validateName(req, res, next) {
+  const { name } = req.body;
+
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+
+  if (name.length < 3) {
+    return res.status(400).json({
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    });
+  }
+
+  next();
+}
+
+function validateAge(req, res, next) {
+  const { age } = req.body;
+
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+
+  if (age < 18) {
+    return res.status(400).json({
+      message: 'A pessoa palestrante deve ser maior de idade',
+    });
+  }
+
+  next();
+}
+
+function validateTalkWatchedAt(req, res, next) {
+  const { talk } = req.body;
+  const dateRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
+  
+  if (!talk || !talk.watchedAt || talk.watchedAt === '') {
+    return res.status(400).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+
+  if (!dateRegex.test(talk.watchedAt)) {
+    return res.status(400).json({
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
+
+  next();
+}
+
+function validateTalkRate(req, res, next) {
+  const { talk } = req.body;
+  const rateRegex = /^[1-5]$/;
+  
+  if (!talk || !talk.rate || talk.rate === '') {
+    return res.status(400).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+
+  if (!rateRegex.test(talk.rate)) {
+    return res.status(400).json({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
+  }
+
+  next();
+}
+
+module.exports = {
+  validateEmail,
+  validatePassword,
+  authenticationToken,
+  validateName,
+  validateAge,
+  validateTalkWatchedAt,
+  validateTalkRate,
+};
