737351532fee5b2eb5710030031b02070d45226c
victorhbm <victor.hugofla@hotmail.com>
2022-03-18 09:27:28 -0300

    feat(index.js): create endpoint GET /talker to get all talkers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { readAFile } = require('./services');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,14 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const data = await readAFile('talker.json');
+
+  if (data) {
+    return res.status(200).json(data);
+  }
 });
 
 app.listen(PORT, () => {
